b84bb6a710a27a534ca6e98bbe4f19f62d9b52cb
Kai Koyamatsu <c011514454@edu.teu.ac.jp>
2019-01-26 21:41:17 +0900

    [remove]buttonを削除

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,6 @@ import { FormGroup, Input, Button } from "reactstrap";
 const Form = props => (
     <form onSubmit={props.handleSubmit}>
         <Input type="text" value={props.text} onChange={props.handleChange} />
-        <Button type="submit" color="primary">Add</Button>
     </form>
 );
 
