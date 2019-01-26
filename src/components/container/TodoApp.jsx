8cda887897bd4e05063a2c38af897cb19d74dcd7
Kai Koyamatsu <c011514454@edu.teu.ac.jp>
2019-01-27 02:13:48 +0900

    [add]入力値がないとエラーを出すように修正

--- src/components/container/TodoApp.jsx
+++ src/components/container/TodoApp.jsx
@@ -6,72 +6,83 @@ import TodoForm from "../presentational/TodoForm";
 import TodoList from "../presentational/TodoList";
 
 class TodoApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: 0,
-      text: "",
-      todos: []
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: 0,
+            text: "",
+            todos: [],
+            error: false
+        };
 
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  handleClick(id) {
-    const todos = this.state.todos.filter(todo => todo.id != id);
-    this.setState({
-      todos: todos
-    });
-  }
+    handleClick(id) {
+        const todos = this.state.todos.filter(todo => todo.id != id);
+        this.setState({
+            todos: todos
+        });
+    }
 
-  handleChange(e) {
-    this.setState({
-      text: e.target.value
-    });
-  }
+    handleChange(e) {
+        this.setState({
+            text: e.target.value
+        });
+    }
 
-  handleSubmit(e) {
-    this.setState(state => ({
-      id: state.id + 1,
-      text: "",
-      todos: [
-        ...state.todos,
-        {
-          id: state.id,
-          text: state.text
+    handleSubmit(e) {
+        e.preventDefault();
+
+        if (!this.state.text) {
+            this.setState(state => ({
+                error: true
+            }));
+            return;
         }
-      ]
-    }));
-    e.preventDefault();
-  }
 
-  render() {
-    return (
-      <Container>
-          <Row>
-              <Col>
-                  <TodoTitle/>
-              </Col>
-          </Row>
-          <Row>
-              <Col>
-                  <TodoForm
-                    text={this.state.text}
-                    handleChange={this.handleChange}
-                    handleSubmit={this.handleSubmit}
-                  />
-              </Col>
-          </Row>
-          <Row>
-              <Col>
-                  <TodoList todos={this.state.todos} handleClick={this.handleClick} />
-              </Col>
-          </Row>
-      </Container>
-    );
-  }
+        this.setState(state => ({
+            id: state.id + 1,
+            text: "",
+            todos: [
+                ...state.todos,
+                {
+                    id: state.id,
+                    text: state.text
+                }
+            ],
+            error: false
+        }));
+    }
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <TodoTitle/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <TodoForm
+                            text={this.state.text}
+                            error={this.state.error}
+                            handleChange={this.handleChange}
+                            handleSubmit={this.handleSubmit}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <TodoList todos={this.state.todos} handleClick={this.handleClick} />
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default TodoApp;
--- src/components/presentational/TodoForm.jsx
+++ src/components/presentational/TodoForm.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import styled from 'styled-components';
-import { Form, Input } from "reactstrap";
+import { Form, Input, Alert } from "reactstrap";
 
 const Wrapper = styled.div`
     margin-bottom: 20px;
 `;
 
-const TodoForm = props => (
-    <Wrapper>
-        <Form onSubmit={props.handleSubmit}>
-            <Input type="text" value={props.text} onChange={props.handleChange} />
-        </Form>
-    </Wrapper>
-);
+const TodoForm = props => {
+    if (props.error) {
+        return (
+            <Wrapper>
+                <Alert color="danger">nothing content</Alert>
+                <Form onSubmit={props.handleSubmit}>
+                    <Input type="text" value={props.text} onChange={props.handleChange} />
+                </Form>
+            </Wrapper>
+        )
+    }
+
+    return (
+        <Wrapper>
+            <Form onSubmit={props.handleSubmit}>
+                <Input type="text" value={props.text} onChange={props.handleChange} />
+            </Form>
+        </Wrapper>
+    )
+};
 
 export default TodoForm;
